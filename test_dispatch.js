77fb644e42d0a13535398a480d39ec435aab9fd4
shubhamwrathcode <shubhamwrathcode@gmail.com>
2026-06-29 10:57:30 +0530

    agce revert files

--- test_dispatch.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let content = fs.readFileSync('src/SocketProvider.js', 'utf8');
-if(content.includes('dispatch(setFuturesData')) {
-  console.log('Already dispatches');
-} else {
-  console.log('Does not dispatch');
-}
